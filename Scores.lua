cdb5402eab30b0e03103aece0ad917c1e1765ad5
itsuncheng <itsuncheng2000@gmail.com>
2016-08-21 07:17:54 +0800

    fix score always zero issue

--- Scores.lua
+++ Scores.lua
@@ -1,16 +1,16 @@
 local json = require( "json" )
-
 local scoreModule={}
-scoreModule.oldTime= nil
-scoreModule.newTime = nil
-scoreModule.elapsedTime = nil
+oldTime= nil
+newTime = nil
+elapsedTime = nil
+
 function scoreModule:start()
-	self.oldTime = os.date( '*t' )
+	oldTime = os.date( '*t' )
 end
 
 function scoreModule:stop()
-	 self.newTime = os.date( '*t' ) 
-	 self.elapsedTime = os.time(newTime) - os.time(oldTime)
+	 newTime = os.date( '*t' ) 
+	 elapsedTime = os.time(newTime) - os.time(oldTime)
 	 print("SCORE: ", elapsedTime )
 	  
 	local function networkListener( event )
@@ -26,7 +26,7 @@ function scoreModule:stop()
 	headers["Content-Type"] = "application/x-www-form-urlencoded"
 	local params = {}
 	params.headers = headers
-	params.body= string.format("name=Raymond&score=%d&levelId=12345678-001&comment=",self.elapsedTime)
+	params.body= string.format("name=Raymond&score=%d&levelId=12345678-001&comment=",elapsedTime)
 	network.request('http://adventure.cloudapp.net/api/record',"POST",networkListener,params)
 end
 return scoreModule
